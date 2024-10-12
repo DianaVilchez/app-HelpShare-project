fd0624a0211afccbf32ebfc492b820a81deb4127
Diana <educ.dana@gmail.com>
2024-10-12 18:14:12 -0500

    feat:Create and implement register view

--- frontend/app-HelpShare-project/src/App.tsx
+++ frontend/app-HelpShare-project/src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css'
 import { Route, Routes } from "react-router-dom"
 import { Login } from "./components/login"
-
+import { Register } from './components/Register'
 function App() {
   return (
   <div className="App">
     <Routes>
             <Route path="/login" element={<Login />} />
+            <Route path="/register"element={<Register />} />
           </Routes>
   </div>
    
