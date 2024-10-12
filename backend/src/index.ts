c395828a78b47ce6013bac3de8567be6ca244608
Gladis Danilu Farill Casiano <danilu080893@hotmail.com>
2024-10-12 10:21:21 -0600

    feat: creation of the connection to the c.r.u.d and the database modeling

---
+++ backend/src/index.ts
@@ -0,0 +1,37 @@
+import express, { Application, Request, Response } from "express";
+import { router } from "./routes/index.routes";
+import dotenv from "dotenv";
+import {testConnection} from './connection';
+import { sequelize } from "./config/sequelize";
+import './models/association.models';
+
+dotenv.config();
+
+const app: Application = express();
+app.use(express.json());
+const port: number = parseInt(process.env.PORT ||"2001", 10);//10 es la base numérica
+
+app.get("/", (req: Request, res: Response): void => {
+  res.send('HelpShare');
+});
+
+router(app);
+
+const startServer = async () => {
+    try {
+      await testConnection();
+      console.log('Database connected');
+
+      await sequelize.sync();
+      console.log('Database synchronized successfully!');
+
+      app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+      });
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+    }
+};
+  
+startServer();
+
