aa45c7cbc60b9fc20186e4312ebfce7687888e23
Antone Gonzales <es.angelika@gmail.com>
2024-10-12 12:59:18 -0500

    feat: setup offer router with getOffers GET route

--- backend/src/controller/offer.controller.ts
+++ backend/src/controller/offer.controller.ts
@@ -1,16 +1,19 @@
-import { Request, Response } from 'express';
-import Offer from '../models/Offers.models';
-import User from '../models/Users.models';
+import { Request, Response } from "express";
+import Offer from "../models/Offers.models";
+import User from "../models/Users.models";
 
-// Crear una nueva oferta 
-export const createOffer = async (req: Request, res: Response): Promise<void> => {
+// Crear una nueva oferta
+export const createOffer = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   try {
     const { title, description, category, id_user } = req.body;
 
-    // Validar que el usuario existe 
+    // Validar que el usuario existe
     const user = await User.findByPk(id_user);
     if (!user) {
-      res.status(404).json({ message: 'User not found' });
+      res.status(404).json({ message: "User not found" });
       return;
     }
 
@@ -20,10 +23,30 @@ export const createOffer = async (req: Request, res: Response): Promise<void> =>
       category,
       id_user, // Asociar la oferta al usuario que la creó
     });
-
+    // console.log("🚀 ~ newOffer:", newOffer)
+    
     res.status(200).json(newOffer);
   } catch (error) {
     console.error("Error creating offer:", error);
-    res.status(500).json({ message: 'Error creating offer' });
+    res.status(500).json({ message: "Error creating offer" });
+  }
+};
+
+// Obtener todas las ofertas
+export const getOffers = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const offers = await Offer.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["username"], // Solo devuelve el campo 'username' del usuario
+        },
+      ],
+    });
+    //   console.log("Offers",offers);
+    res.status(200).json(offers);
+  } catch (error) {
+    console.error("Error fetching offers:", error);
+    res.status(500).json({ message: "Error fetching offers" });
   }
 };
--- backend/src/routes/offers.routes.ts
+++ backend/src/routes/offers.routes.ts
@@ -1,9 +1,11 @@
 import express, { Router } from 'express';
-import { createOffer } from '../controller/offer.controller';
+import { createOffer, getOffers } from '../controller/offer.controller';
 
 const routerOffers: Router = express.Router();
 
 routerOffers.post('/', createOffer);
+
+routerOffers.get('/', getOffers);
 
 
 export { routerOffers};
