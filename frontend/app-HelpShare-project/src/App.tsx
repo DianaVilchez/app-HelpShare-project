1a5bad66525d799614bbbc65eed36b6d5665ca0e
Gladis Danilu Farill Casiano <danilu080893@hotmail.com>
2024-10-12 23:46:31 -0600

    feat: creation of the search engine and added the search engine view

--- frontend/app-HelpShare-project/src/App.tsx
+++ frontend/app-HelpShare-project/src/App.tsx
@@ -1,13 +1,14 @@
-import './App.css'
 import { Route, Routes } from "react-router-dom"
-import { Login } from "./components/login"
-import { Register } from './components/Register'
+import { Login } from "./components/Login";
+import { Register } from './components/Register';
+import { Search } from "./components/Search";
 function App() {
   return (
   <div className="App">
     <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/register"element={<Register />} />
+            <Route path="/search"element={<Search />} />
           </Routes>
   </div>
    
--- frontend/app-HelpShare-project/src/main.tsx
+++ frontend/app-HelpShare-project/src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import './styles/login.css'
 import './styles/register.css'
-import './styles/profile.css'
+import './styles/search.css'
+// import './styles/profile.css'
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
